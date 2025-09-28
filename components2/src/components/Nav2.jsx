0d550a0a92260bae90836856e64b61334d678d78
Janvi1205 <janviyadav120505@gmail.com>
2025-09-28 16:41:37 +0530

    Components within components

---
+++ components2/src/components/Nav2.jsx
@@ -0,0 +1,13 @@
+const Nav2=()=>{
+    return(
+         <div style={{display:"flex",gap:"60px",color:"white"}}>
+               <h2>Home</h2>
+               <h2>About</h2>
+               <h2>Contact </h2>
+              <h2>Projects</h2>
+
+            </div>
+
+    )
+}
+export default Nav2;
---
+++ components2/src/components/Navbar.jsx
@@ -0,0 +1,11 @@
+import Nav2 from './Nav2'
+const Nav=()=>{
+    return(
+        <nav style={{display:"flex",justifyContent:"space-between", backgroundColor:"green", padding:20}}>
+            <h1 style={{color:"white"}} >Janvi</h1>
+           
+             <Nav2/>
+        </nav>
+    )
+}
+export default Nav;
