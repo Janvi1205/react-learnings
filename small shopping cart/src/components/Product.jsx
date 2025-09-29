9ddfc84abe74f9b87961500f929dc394e632bb9c
Janvi1205 <janviyadav120505@gmail.com>
2025-09-29 23:31:09 +0530

    improved styling and UI of the page and cards

--- small shopping cart/src/components/Product.jsx
+++ small shopping cart/src/components/Product.jsx
@@ -1,10 +1,10 @@
 const Product=(props)=>{
     return (
-        <div style={{display:"inline-block",marginLeft:"80px",border:"1px solid black",padding:"20px",width:"200px",marginTop:"20px", textAlign:"center"}}>
+        <div style={{display:"inline-block",marginLeft:"80px",border:"1px solid black",padding:"20px",width:"200px",marginTop:"20px", textAlign:"center",borderRadius:"10px",backgroundColor:"rgb(249, 245, 240)"}}>
             <img style={{width:"80px",height:"80px",borderRadius:"100px"}} src={props.image} alt="" />
             <h1 style={{fontSize:"30px",fontFamily:"Arial"}}>{props.name}</h1>
             <h2>Price:{props.price}</h2>
-            <button style={{padding:"12px",width:"160px",borderRadius:"20px"}}>Add to cart</button>
+            <button style={{backgroundColor:"black",color:"white",padding:"12px",width:"160px",borderRadius:"20px"}}>Add to cart</button>
         </div>
 
     )
