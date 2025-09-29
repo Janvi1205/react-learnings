58296c93d3523ec15a37ace42ef361dfb35893b2
Janvi1205 <janviyadav120505@gmail.com>
2025-09-29 22:57:05 +0530

    updated the images

--- small shopping cart/src/App.jsx
+++ small shopping cart/src/App.jsx
@@ -18,49 +18,49 @@ const App=()=>{
     id: 3, 
     name: "Mango", 
     price: 70, 
-    image: "https://images.unsplash.com/photo-1627308595216-439b63e3e5ff?w=400" 
+    image: "public/images/mango.png"
   },
   { 
     id: 4, 
     name: "Orange", 
     price: 40, 
-    image: "https://images.unsplash.com/photo-1611080626919-7c8c6b3c3a89?w=400" 
+    image: "public/images/orange.png"
   },
   { 
     id: 5, 
     name: "Grapes", 
     price: 60, 
-    image: "https://images.unsplash.com/photo-1615486364205-8af0d8f8b7a1?w=400" 
+    image: "public/images/grapes.png"
   },
   { 
     id: 6, 
     name: "Pineapple", 
     price: 90, 
-    image: "https://images.unsplash.com/photo-1502741126161-b048400d0809?w=400" 
+    image: "public/images/pine.png" 
   },
   { 
     id: 7, 
     name: "Watermelon", 
     price: 120, 
-    image: "https://images.unsplash.com/photo-1610440042657-5d96b9f2de3d?w=400" 
+    image: "public/images/watermelon.png"
   },
   { 
     id: 8, 
     name: "Strawberry", 
     price: 150, 
-    image: "https://images.unsplash.com/photo-1567306226416-28f0efdc88ce?w=400" 
+    image: "public/images/stawb.png" 
   },
   { 
     id: 9, 
     name: "Kiwi", 
     price: 80, 
-    image: "https://images.unsplash.com/photo-1615484477214-5d6d6f90e1f1?w=400" 
+    image: "public/images/kiwi.png",
   },
   { 
     id: 10, 
     name: "Papaya", 
     price: 55, 
-    image: "https://images.unsplash.com/photo-1611080626919-7c8c6b3c3a89?w=400" 
+    image: "public/images/papaya.png",
   }
 ];
 
