853f1ad94b3bd0a97e77116d7a618e575d093b21
Janvi1205 <janviyadav120505@gmail.com>
2025-09-30 10:10:45 +0530

    Updated UI

--- small shopping cart/src/App.jsx
+++ small shopping cart/src/App.jsx
@@ -1,75 +1,86 @@
 import Product from "./components/Product";
-const App=()=>{
+import Nav from "./components/Nav";
+const App = () => {
   const products = [
-  { 
-    id: 1, 
-    name: "Apple", 
-    price: 50, 
-    image: "https://images.unsplash.com/photo-1567306226416-28f0efdc88ce?w=400" 
-  },
-  { 
-    id: 2, 
-    name: "Banana", 
-    price: 20, 
-    image: "public/images/banana.png"
-    
-  },
-  { 
-    id: 3, 
-    name: "Mango", 
-    price: 70, 
-    image: "public/images/mango.png"
-  },
-  { 
-    id: 4, 
-    name: "Orange", 
-    price: 40, 
-    image: "public/images/orange.png"
-  },
-  { 
-    id: 5, 
-    name: "Grapes", 
-    price: 60, 
-    image: "public/images/grapes.png"
-  },
-  { 
-    id: 6, 
-    name: "Pineapple", 
-    price: 90, 
-    image: "public/images/pine.png" 
-  },
-  { 
-    id: 7, 
-    name: "Watermelon", 
-    price: 120, 
-    image: "public/images/watermelon.png"
-  },
-  { 
-    id: 8, 
-    name: "Strawberry", 
-    price: 150, 
-    image: "public/images/stawb.png" 
-  },
-  { 
-    id: 9, 
-    name: "Kiwi", 
-    price: 80, 
-    image: "public/images/kiwi.png",
-  },
-  { 
-    id: 10, 
-    name: "Papaya", 
-    price: 55, 
-    image: "public/images/papaya.png",
-  }
-];
+    {
+      id: 1,
+      name: "Apple",
+      price: 50,
+      image: "https://images.unsplash.com/photo-1567306226416-28f0efdc88ce?w=400"
+    },
+    {
+      id: 2,
+      name: "Banana",
+      price: 20,
+      image: "public/images/banana.png"
 
-     return (
+    },
+    {
+      id: 3,
+      name: "Mango",
+      price: 70,
+      image: "public/images/mango.png"
+    },
+    {
+      id: 4,
+      name: "Orange",
+      price: 40,
+      image: "public/images/orange.png"
+    },
+    {
+      id: 5,
+      name: "Grapes",
+      price: 60,
+      image: "public/images/grapes.png"
+    },
+    {
+      id: 6,
+      name: "Pineapple",
+      price: 90,
+      image: "public/images/pine.png"
+    },
+    {
+      id: 7,
+      name: "Watermelon",
+      price: 120,
+      image: "public/images/watermelon.png"
+    },
+    {
+      id: 8,
+      name: "Strawberry",
+      price: 150,
+      image: "public/images/stawb.png"
+    },
+    {
+      id: 9,
+      name: "Kiwi",
+      price: 80,
+      image: "public/images/kiwi.png",
+    },
+    {
+      id: 10,
+      name: "Papaya",
+      price: 55,
+      image: "public/images/papaya.png",
+    }
+  ];
+
+  return (
     <div>
-      {products.map(elem => (
-        <Product  name={elem.name} price={elem.price} image={elem.image} />
-      ))}
+      <Nav/>
+
+      <div>
+
+        {products.map(elem => (
+          <Product name={elem.name} price={elem.price} image={elem.image} />
+        ))}
+      </div>
+
+
+
     </div>
+
+
   );
 }
 export default App;
--- small shopping cart/src/components/Product.jsx
+++ small shopping cart/src/components/Product.jsx
@@ -1,6 +1,6 @@
 const Product=(props)=>{
     return (
-        <div style={{display:"inline-block",marginLeft:"80px",border:"1px solid black",padding:"20px",width:"200px",marginTop:"20px", textAlign:"center",borderRadius:"10px",backgroundColor:"rgb(249, 245, 240)"}}>
+        <div style={{display:"inline-block",marginLeft:"85px",border:"1px solid black",padding:"20px",width:"200px",marginTop:"20px", textAlign:"center",borderRadius:"10px",backgroundColor:"rgb(249, 245, 240)"}}>
             <img style={{width:"80px",height:"80px",borderRadius:"100px"}} src={props.image} alt="" />
             <h1 style={{fontSize:"30px",fontFamily:"Arial"}}>{props.name}</h1>
             <h2>Price:{props.price}</h2>
