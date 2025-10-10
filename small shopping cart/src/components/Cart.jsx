7edfda12c71c9425d1b2437fa287d57f72611a53
Janvi1205 <janviyadav120505@gmail.com>
2025-10-10 19:06:49 +0530

    did styling of card component

--- small shopping cart/src/components/Cart.jsx
+++ small shopping cart/src/components/Cart.jsx
@@ -1,9 +1,13 @@
 
 const Cart=({filteredobj})=>{
     return (
-        <div>
-            <div style={{height:"200px"}}>
+        <div style={{display:"flex"}}>
+            <div className="addtocartsection" >
 
+            </div>
+            <div className="addtocartsection2">
+                <h1 style={{fontSize:"40px",color:"white",fontFamily:"Arial", justifyContent:"center",display:"flex"}}>Total</h1>
+   
             </div>
         </div>
       
