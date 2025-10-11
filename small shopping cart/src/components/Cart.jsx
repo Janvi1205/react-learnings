374c049f4448453dc9e2909155bc288fb3089f71
Janvi1205 <janviyadav120505@gmail.com>
2025-10-11 18:59:29 +0530

    Added more functionality

--- small shopping cart/src/components/Cart.jsx
+++ small shopping cart/src/components/Cart.jsx
@@ -1,66 +1,87 @@
-import { useState } from "react";
 import { MdDelete } from "react-icons/md";
 const Cart = ({ setcartprod, cartprod }) => {
     console.log("cartprod", cartprod);
 
-    
-    function incqty(id)
-    {
-        setcartprod((prevcart)=>
-            prevcart.map((item)=>item.id==id?{ ...item, count: (item.count || 1) + 1 }:item)
+
+    function incqty(id) {
+        setcartprod((prevcart) =>
+            prevcart.map((item) => item.id == id ? { ...item, count: (item.count || 1) + 1 } : item)
 
         )
 
     }
-   
-    function decqty(id)
-    {
-        setcartprod((prevcart)=>
-            prevcart.map((item)=>item.id==id?{ ...item, count:item.count>1?item.count-1:1 }:item)
+
+    function decqty(id) {
+        setcartprod((prevcart) =>
+            prevcart.map((item) => item.id == id ? { ...item, count: item.count > 1 ? item.count - 1 : 1 } : item)
 
         )
-       
+
 
     }
 
-     function deleteItem(id) {
-           setcartprod((prevcart) => prevcart.filter((item) => item.id !== id));
+    function deleteItem(id) {
+        setcartprod((prevcart) => prevcart.filter((item) => item.id !== id));
     }
 
-    
+
+    const total = cartprod.reduce((sum, item) => {
+        const qty = item.count || 1;
+        return sum + item.price * qty;
+    }, 0);
+
+
+
     return (
-        <div style={{ display: "flex" }}>
-            <div className="addtocartsection">
-                {cartprod.map((item) => {
-                    const qty = item.count || 1; 
-                    return (
-                        <div key={item.id} style={{ height: "300px", backgroundColor: "white", width: "850px",display:"flex",marginTop:"45px",marginLeft:"75px",borderRadius:"40px" }}>
-                            <img style={{height:"220px",width:"250px",marginTop:"35px",marginLeft:"20px"}} src={item.image} alt="" />
-                            <div>
-                                <h1 style={{fontFamily:"Arial",marginLeft:"40px",marginTop:"40px",fontSize:"40px"}}>{item.name}</h1>
-                                <h2 style={{fontSize:"25px",fontFamily:"Arial",marginLeft:"40px"}}>Price:{item.price*qty}</h2>
-                                
-                                <button   onClick={()=>decqty(item.id)} className="dec">-</button>
-                                <span style={{fontSize:"25px",marginLeft:"30px"}}>{qty}</span>
-                                <button onClick={()=>incqty(item.id)}  className="inc">+</button>
-                                <button onClick={()=>deleteItem(item.id)} style={{backgroundColor:"red",border:"none",padding:"7px",fontSize:"30px",marginLeft:"280px",marginTop:"30px",borderRadius:"10px"}}><MdDelete /></button>
+        <div >
+            {cartprod.length === 0 ? (
+                <div style={{color:"white",fontSize:"45px",marginLeft:"650px",marginTop:"250px"}}>Oops!No item in cart...</div>
 
-                                
+            ) : (
+                <div style={{ display: "flex" }}>
+                    <div className="addtocartsection">
+                        {cartprod.map((item) => {
+                            const qty = item.count || 1;
+                            return (
+                                <div key={item.id} style={{ height: "300px", backgroundColor: "white", width: "850px", display: "flex", marginTop: "45px", marginLeft: "75px", borderRadius: "40px" }}>
+                                    <img style={{ height: "220px", width: "250px", marginTop: "35px", marginLeft: "20px" }} src={item.image} alt="" />
+                                    <div>
+                                        <h1 style={{ fontFamily: "Arial", marginLeft: "40px", marginTop: "40px", fontSize: "40px" }}>{item.name}</h1>
+                                        <h2 style={{ fontSize: "25px", fontFamily: "Arial", marginLeft: "40px" }}>Price:{item.price * qty}</h2>
 
-                            </div>
-                          
-                            
-                        </div>
-                    );
-                })}
+                                        <button onClick={() => decqty(item.id)} className="dec">-</button>
+                                        <span style={{ fontSize: "25px", marginLeft: "30px" }}>{qty}</span>
+                                        <button onClick={() => incqty(item.id)} className="inc">+</button>
+                                        <button onClick={() => deleteItem(item.id)} style={{ backgroundColor: "red", border: "none", padding: "7px", fontSize: "30px", marginLeft: "280px", marginTop: "30px", borderRadius: "10px" }}><MdDelete /></button>
 
 
-            </div>
 
-             <div className="addtocartsection2">
+                                    </div>
 
 
-             </div>
+                                </div>
+                            );
+                        })}
+
+
+                    </div>
+
+                    <div className="addtocartsection2">
+                        <h1 style={{ fontSize: "35px", justifyContent: "center", display: "flex", }}>Bill</h1>
+                        <p style={{ fontSize: "27px", marginLeft: "30px", marginTop: "40px", color: "white", fontFamily: "Arial" }}> Total amount to be paid:{total}</p>
+
+
+
+                    </div>
+
+                </div>
+
+            )}
+
+
+
+
+
         </div>
     );
 };
--- small shopping cart/src/components/Product.jsx
+++ small shopping cart/src/components/Product.jsx
@@ -3,9 +3,13 @@ import { products } from "../../data";
 
 const Product = ({setproduct}) => {
 
+
     function addtocart(id){
 
        setproduct(id);
+    
+
+       
     }
 
     return (
@@ -17,14 +21,14 @@ const Product = ({setproduct}) => {
                     <h1 style={{ fontSize: "30px", fontFamily: "Arial" }}>{item.name}</h1>
                     <h2>Price:{item.price}</h2>
 
-                    <button onClick={()=>addtocart(item.id)} style={{ backgroundColor: "black", color: "white", padding: "10px", width: "140px", borderRadius: "20px", cursor: "pointer" }}>
+                    
+
+                    <button  onClick={()=>addtocart(item.id)} style={{ backgroundColor: "black", color: "white", padding: "10px", width: "140px", borderRadius: "20px", cursor: "pointer" }}>
                         Add to cart
                     </button>
 
 
-                    <button style={{ display:"none", fontSize: "26px", borderRadius: "20px", width: "40px", marginRight: "30px" }} >-</button>
-                    <span style={{ display:"none", fontSize: "26px" }}>sds</span>
-                    <button style={{ display:"none", fontSize: "26px", borderRadius: "20px", width: "40px", marginLeft: "30px" }}>+</button>
+                    
 
                 </div>
 
