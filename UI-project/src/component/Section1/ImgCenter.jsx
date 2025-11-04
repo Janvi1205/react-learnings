e3ce6265bec0afb72326175a816b14560f4c35d9
Janvi1205 <janviyadav120505@gmail.com>
2025-11-04 21:18:29 +0530

    completed a basic ui project

--- UI-project/src/component/Section1/ImgCenter.jsx
+++ UI-project/src/component/Section1/ImgCenter.jsx
@@ -1,9 +1,31 @@
-const ImgCenter=()=>{
+const ImgCenter = () => {
     return (
         <div>
-            <div>
-                <div className="">
-                    <img src="" alt="" />
+            <div className="flex gap-5 mt-10  ">
+                <div className="w-80" >
+                    
+                    <div className="rounded-4xl h-10 w-10 bg-white absolute flex justify-center items-center font-medium text-2xl mt-7 ml-5  ">1</div>
+                     <div className=" mt-80 ml-7 font-bold text-[20px]  absolute text-white text-wrap">Lorem ipsum dolor <br /> sit amet consectetur adipisi <br /> Possimus, ipsam. Lorem  <br />ipsum dolor sit amet cons</div>
+                    <img className="h-150 w-80  rounded-2xl" src="https://images.unsplash.com/photo-1562593528-e5ba9e5abb78?ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&q=80&w=687" alt="" />
+                  <button className="bg-orange-600 p-3 -mt-28 ml-7 absolute text-white rounded-4xl text-xl">Satisfied</button>
+                   
+                    
+                </div>
+                <div >
+                    <div className="rounded-4xl h-10 w-10 bg-white absolute flex justify-center items-center font-medium text-2xl mt-7 ml-5  ">2</div>
+                    <div className=" mt-80 ml-7 font-bold text-[20px]  absolute text-white text-wrap">Lorem ipsum dolor <br /> sit amet consectetur adipisi <br /> Possimus, ipsam. Lorem  <br />ipsum dolor sit amet cons</div>
+
+                    <img className="h-150  w-80 rounded-2xl" src="https://images.unsplash.com/photo-1708533295162-dc25d26719af?ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&q=80&w=687" alt="" />
+                     <button className="bg-orange-600 p-3 -mt-28 ml-7 absolute text-white rounded-4xl text-xl">Satisfied</button>
+                   
+                </div>
+                <div >
+                    <div className="rounded-4xl h-10 w-10 bg-white absolute flex justify-center items-center font-medium text-2xl mt-7 ml-5  ">3</div>
+                    <div className=" mt-80 ml-7 font-bold text-[20px]  absolute text-white text-wrap">Lorem ipsum dolor <br /> sit amet consectetur adipisi <br /> Possimus, ipsam. Lorem  <br />ipsum dolor sit amet cons</div>
+
+                    <img className="h-150  w-80 mr-25 rounded-2xl" src="https://images.unsplash.com/photo-1720190370264-15c444358c76?ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&q=80&w=627" alt="" />
+                     <button className="bg-orange-600 p-3 -mt-28 ml-7 absolute text-white rounded-4xl text-xl">Satisfied</button>
+                   
                 </div>
             </div>
 
